30686550461878063fb1547c4216ac9773e410e7
Miheretab Alemu <mihrtab@gmail.com>
2022-06-06 20:48:01 +0300

    initialize test

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    </head>
+    <body>
+        <h1 style='text-align: center;'>Welcome to Miheretab server</h1>
+        <p style='text-align: center;'>To run you project here, Put your project folder in <b style='color: red;'>/var/www/html/</b> and make sure the public folder is at <b style='color: red;'>/var/www/html/public/</b> path.</p>
+    </body>
+</html>
